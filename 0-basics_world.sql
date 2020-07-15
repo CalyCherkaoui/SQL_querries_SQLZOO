51f0e34e080fa859b76090547386b0df7e117e9e
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-15 13:13:35 -0500

    Sql querries for the tutorial 1  querry 5 6 7

--- 0-basics_world.sql
+++ 0-basics_world.sql
@@ -1,5 +1,6 @@
 /* Tutorial 0 Some simple queries to get you started */
 /* ***************************************************/
+
 /* 1 - The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) 
 should be in 'single quotes'; Modify it to show the population of Germany*/
 
--- 1-Patern_matching_strings.sql
+++ 1-Patern_matching_strings.sql
@@ -15,9 +15,34 @@ SELECT name
 FROM   world
 WHERE  name LIKE '%y';
 
-/* 3 - Iceland, Switzerland end with land - but are there others?
+/* 3 - Luxembourg has an x - so does one other country. List them both.
+Find the countries that contain the letter x */
+
+SELECT name
+FROM   world
+WHERE  name LIKE '%x%';
+
+/* 4 - Iceland, Switzerland end with land - but are there others?
 Find the countries that end with land */
 
 SELECT name
 FROM   world
 WHERE  name LIKE '%land';
+
+/* 5 - Columbia starts with a C and ends with ia - there are two more like this.
+Find the countries that start with C and end with ia */
+
+SELECT name 
+FROM   world
+WHERE  name LIKE 'C%' AND name LIKE '%ia';
+
+/* 6 - Greece has a double e - who has a double o?
+Find the country that has oo in the name */
+
+SELECT name
+FROM   world
+WHERE  name LIKE '%oo%';
+
+/* 7 - Bahamas has three a - who else?
+Find the countries that have three or more a in the name */
+
