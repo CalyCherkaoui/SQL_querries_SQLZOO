2c9f26d72745072ade5006c5a2ac341d572c7561
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-21 10:21:08 -0500

    Sql querries for the tutorial 4

--- 4-Nested_select.sql
+++ 4-Nested_select.sql
@@ -112,5 +112,20 @@ Find the continents where all countries have a population <= 25000000. Then find
 names of the countries associated with these continents. Show name, continent and 
 population. */
 
+SELECT name, continent, population
+  FROM world x
+ WHERE 25000000 >= ALL(SELECT population
+                     FROM world y
+                    WHERE y.continent=x.continent
+                    AND y.population > 0);
+
 /* 10 - Some countries have populations more than three times that of any of their 
 neighbours (in the same continent). Give the countries and continents.*/
+
+SELECT name, continent
+  FROM world x
+ WHERE population > ALL(SELECT 3*population
+                     FROM world y
+                    WHERE y.continent=x.continent
+                    AND y.population > 0
+                    AND x.name != y.name);
