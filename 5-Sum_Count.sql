b008c504e46722c6d6e4c9524053bd0269475283
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-21 10:40:47 -0500

    Sql querries for the tutorial 5

--- 5-Sum_Count.sql
+++ 5-Sum_Count.sql
@@ -17,23 +17,43 @@ SELECT DISTINCT(continent)
 
 /* 3 - Give the total GDP of Africa */
 
-SELECT DISTINCT(continent)
-  FROM world;
+SELECT SUM(gdp) AS afri_gdp
+  FROM world
+  WHERE continent = 'Africa';
 
 /* 4 - Count the big countries*/
 
-SELECT DISTINCT(continent)
-  FROM world;
+SELECT COUNT(name)
+  FROM world
+ WHERE area >= 1000000;
 
 /* 5 - Baltic states population 
 What is the total population of ('Estonia', 'Latvia', 'Lithuania') */
 
+SELECT SUM(population) AS baltic_pop
+  FROM world
+ WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
 /* 6 Using GROUP BY and HAVING - 
 For each continent show the continent and number of countries */
 
+SELECT continent, COUNT(name)
+  FROM world
+ GROUP BY continent; 
+
 /* 7 - Counting big countries in each continent 
-For each continent show the continent and number of countries with populations of at least 10 million.*/
+For each continent show the continent and number of countries with populations of at
+ least 10 million.*/
+
+ SELECT continent, COUNT(name)
+   FROM world
+  WHERE population >= 10000000
+  GROUP BY continent; 
 
 /* 8 - Counting big continents
 List the continents that have a total population of at least 100 million. */
 
+ SELECT continent
+   FROM world
+  GROUP BY continent
+  HAVING SUM(population) > 100000000; 
