b80928e3534dbd44e2a4a59d08444c23d810742d
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-21 16:12:21 -0500

    Sql querries for the tutorial 6

--- 6-Join_two_tables.sql
+++ 6-Join_two_tables.sql
@@ -70,5 +70,50 @@ eteam.id instead of just id
 List the dates of the matches and the name of the team in which 'Fernando Santos' was 
 the team1 coach.*/
 
+SELECT mdate, teamname
+  FROM game
+  JOIN eteam
+    ON team1=eteam.id
+ WHERE coach LIKE 'Fernando Santos';
+
 /* 7 - List the player for every goal scored in a game where the stadium was 'National 
 Stadium, Warsaw'*/
+
+SELECT player
+ FROM goal
+ JOIN game
+ ON id=matchid
+ WHERE stadium LIKE 'National Stadium, Warsaw';
+
+/* 8 - 
+The example query shows all goals scored in the Germany-Greece quarterfinal.
+Instead show the name of all players who scored a goal against Germany.
+HINT
+Select goals scored only by non-German players in matches where GER was the id of
+ either team1 or team2.
+You can use teamid!='GER' to prevent listing German players.
+You can use DISTINCT to stop players being listed twice.*/
+
+/* 9 -
+Show teamname and the total number of goals scored.
+COUNT and GROUP BY*/
+
+/* 10 - Show the stadium and the number of goals scored in each stadium. */
+
+/* 11 -
+For every match involving 'POL', show the matchid, date and the number of goals scored.*/
+
+/* 12 - or every match where 'GER' scored, show matchid, match date and the number of
+ goals scored by 'GER'*/
+
+/* 13 - List every match with the goals scored by each team as shown. This will use 
+"CASE WHEN" which has not been explained in any previous exercises.
+mdate	        team1	score1	team2	score2
+1 July 2012	  ESP	  4	      ITA 	0
+10 June 2012	ESP	  1	      ITA	  1
+10 June 2012	IRL	  1	      CRO	  3
+Notice in the query given every goal is listed. If it was a team1 goal then a 1 appears
+ in score1, otherwise there is a 0. You could SUM this column to get a count of the 
+ goals scored by team1. Sort your result by mdate, matchid, team1 and team2.*/
+
+
