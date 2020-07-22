a1e5b24e706e77b6f77d3017b501eb51ff67e6f2
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-22 12:31:49 -0500

    Sql querries for the tutorial 7

--- 7-Join_Operation.sql
+++ 7-Join_Operation.sql
@@ -162,6 +162,15 @@ SELECT name
 /* 14 - List the films released in the year 1978 ordered by the number of actors in the
  cast, then by title. */
 
+SELECT title, COUNT(casting.actorid) as num_actors
+ FROM movie
+ JOIN casting
+   ON movie.id = casting.movieid
+ WHERE movie.yr = 1978
+ GROUP BY movie.id
+ ORDER BY num_actors DESC
+ ORDER BY title;
+
 /* 15 - List all the people who have worked with 'Art Garfunkel'. */
 
 
