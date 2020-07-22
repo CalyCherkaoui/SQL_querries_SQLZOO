f076aade460c79fd0fdffccc3739e4505b33cb37
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-22 14:26:54 -0500

    Sql querries for the tutorial 8

--- 8-Using_Null.sql
+++ 8-Using_Null.sql
@@ -1,11 +1,21 @@
-/*                      Tuto 7 - JOIN OPERATION.                     */
+/*                      Tuto 8 - USING_NULL.                     */
 /*                                                                   */
+-- table teacher (id 	dept 	name 	phone 	mobile)
+-- table dep (id 	name)
+
+-- COALESCE , INNER JOIN, LEFT JOIN, RIGHT JOIN, IS NULL
 /* https://napier.sqlzoo.net/wiki/Using_Null                         */
 /* *******************************************************************/
 
 /* 1 - NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN 
 List the teachers who have NULL for their department.
-Why we cannot use = */
+Why we cannot use = 
+You might think that the phrase dept=NULL would work here but it doesn't
+ - you can use the phrase dept IS NULL*/
+
+SELECT name
+  FROM teacher
+ WHERE dept is NULL;
 
 /* 2 - Note the INNER JOIN misses the teachers with no department and the departments 
 with no teacher.*/
@@ -16,13 +26,30 @@ SELECT teacher.name, dept.name
 
 /* 3 - Use a different JOIN so that all teachers are listed. */
 
+SELECT teacher.name, dept.name
+  FROM teacher 
+  LEFT JOIN dept
+    ON (teacher.dept=dept.id);
+
 /* 4 - Use a different JOIN so that all departments are listed. */
 
+SELECT teacher.name, dept.name
+  FROM teacher 
+ RIGHT JOIN dept
+    ON (teacher.dept=dept.id);
+
 /* 5 - Use COALESCE to print the mobile number. Use the number '07986 444 2266'
- if there is no number given. Show teacher name and mobile number or '07986 444 2266'*/
+if there is no number given. Show teacher name and mobile number or '07986 444 2266'*/
+
+SELECT teacher.name, COALESCE(teacher.mobile,'07986 444 2266')
+  FROM teacher;
 
 /* 6 - Use the COALESCE function and a LEFT JOIN to print the teacher name and 
 department name. Use the string 'None' where there is no department. */
+
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher
+LEFT JOIN dept ON dept.id = teacher.dept;
 
 /* 7 - Use COUNT to show the number of teachers and the number of mobile phones*/
 
