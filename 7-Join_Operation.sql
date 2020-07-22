631a3b6fbabcdb120131fbfc44cc022780a7e04c
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-22 09:45:49 -0500

    Sql querries for the tutorial 7

--- 7-Join_Operation.sql
+++ 7-Join_Operation.sql
@@ -10,10 +10,23 @@
 /* 1 1962 movies
 List the films where the yr is 1962 [Show id, title] */
 
+SELECT id, title
+  FROM movie
+ WHERE yr=1962; 
+
 /* 2 - Give year of 'Citizen Kane'. */
+
+SELECT yr
+  FROM movie
+ WHERE title LIKE 'Citizen Kane';
 
 /* 3 - List all of the Star Trek movies, include the id, title and yr (all of these movies
  include the words Star Trek in the title). Order results by year. */
+
+SELECT id, title, yr
+  FROM movie
+ WHERE title LIKE '%Star Trek%'
+ ORDER BY yr;
 
 /* 4 - What id number does the actor 'Glenn Close' have? */
 
