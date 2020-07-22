8644cb00d4af46eb0bfbe5cb4b461bd4435c5bda
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-22 12:52:15 -0500

    Sql querries for the tutorial 7

--- 8-Using_Null.sql
+++ 8-Using_Null.sql
@@ -18,4 +18,20 @@ SELECT teacher.name, dept.name
 
 /* 4 - Use a different JOIN so that all departments are listed. */
 
+/* 5 - Use COALESCE to print the mobile number. Use the number '07986 444 2266'
+ if there is no number given. Show teacher name and mobile number or '07986 444 2266'*/
+
+/* 6 - Use the COALESCE function and a LEFT JOIN to print the teacher name and 
+department name. Use the string 'None' where there is no department. */
+
+/* 7 - Use COUNT to show the number of teachers and the number of mobile phones*/
+
+/* 8 - Use COUNT and GROUP BY dept.name to show each department and the number of
+ staff. Use a RIGHT JOIN to ensure that the Engineering department is listed. */
+
+/* 9 - Use CASE to show the name of each teacher followed by 'Sci' if the teacher is 
+in dept 1 or 2 and 'Art' otherwise. 
+
+/* 10 - Use CASE to show the name of each teacher followed by 'Sci' if the teacher 
+is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise. */
 
